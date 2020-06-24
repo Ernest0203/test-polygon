d48207aa21554faad810e907e0e367f2fa1a1f90
Ernest0203 <ernest2332@gmail.com>
2020-06-24 12:52:53 +0300

    added test chart

--- server/migrations/20200227100252-initial-migration.js
+++ server/migrations/20200227100252-initial-migration.js
@@ -2,12 +2,89 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('modelName', {
-
+    return queryInterface.createTable('Test1', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+        field: "created_at"
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+        onUpdate : Sequelize.literal('CURRENT_TIMESTAMP'),
+        field: "updated_at"
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        onDelete : Sequelize.literal('CURRENT_TIMESTAMP'),
+        field: "deleted_at"
+      }
     })
+    .then(() => queryInterface.createTable('Test2', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+        field: "created_at"
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+        onUpdate : Sequelize.literal('CURRENT_TIMESTAMP'),
+        field: "updated_at"
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        onDelete : Sequelize.literal('CURRENT_TIMESTAMP'),
+        field: "deleted_at"
+      }
+    }))
+    .then(() => queryInterface.createTable('Test3', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+        field: "created_at"
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+        onUpdate : Sequelize.literal('CURRENT_TIMESTAMP'),
+        field: "updated_at"
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        onDelete : Sequelize.literal('CURRENT_TIMESTAMP'),
+        field: "deleted_at"
+      }
+    }))
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('modelName')
+    return queryInterface.dropTable('Test1')
+      .then(() => queryInterface.dropTable('Test2'))
+      .then(() => queryInterface.dropTable('Test3'))
   }
 };
